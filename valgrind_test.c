529ba9ac1203a178f4ecc196a7b858cd65d774ad
Marshall Asch <maasch@rogers.com>
2020-01-17 18:04:32 -0500

    added credit

--- valgrind_test.c
+++ valgrind_test.c
@@ -1,12 +1,17 @@
+/**
+ * This code is duplicated from:
+ * https://valgrind.org/docs/manual/quick-start.html
+ */
+
 #include <stdlib.h>
 
 void f(void) {
-  int* x = malloc(10 * sizeof(int));
-  x[10] = 0;
+	int* x = malloc(10 * sizeof(int));
+	x[10] = 0;
 }
 
 int main(void) {
-  f();
-  return 0;
+	f();
+	return 0;
 }
 
